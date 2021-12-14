cd49151922f5fb33072781c71fc17fbac6d781f7
Stryker101 <onuoha.henrychika@gmail.com>
2021-12-14 12:53:07 +0100

    first commit

--- ALGO.js
+++ ALGO.js
@@ -4,34 +4,32 @@
 
 
 // QUESTION 1
-let test = [1,2,3,4]
-function productOfElements(test, index) {
+
+function findProduct(arr) {
+  function productOfElements(arr, index) {
     let product = 1;
-    for (i = index; i < test.length; i++) {
-      product *= test[i];
+    for (i = index; i < arr.length; i++) {
+      product *= arr[i];
     }
     return product;
   }
-function findProduct(test) {
     let newArray = 1;
     let productArray = [];
-    for(let i=0; i<test.length; i++) {
-        productArray[i] = newArray * productOfElements(test, i+1);
-        newArray *= test[i];
+    for(let i=0; i<arr.length; i++) {
+        productArray[i] = newArray * productOfElements(arr, i+1);
+        newArray *= arr[i];
     }
     return productArray;
 }
-console.log(findProduct(test))
-
-
+console.log(findProduct(arr))
 
 
 
 // QUESTION 2
-array1 = [1,2,4,5,10,6,3];
-array2 = [0,8,19,90,11,16,13];
-newArray = array1.concat(array2);
-function sortElements(a,b) {
-  return a-b;
-}
-console.log(newArray.sort(sortElements))
+function mergeArray (array1, array2) {
+
+  let mergeArray = array1.concat(array2).sort((a, b) => a-b);
+  return mergeArray
+   
+ }
+ console.log(mergeArray(array1, array2))
